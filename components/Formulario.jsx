985793010adc5746034e1b6afc46c08461caa5bf
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-13 18:50:14 -0300

    se agrega librería SweetAlert

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
+import Swal from 'sweetalert2'
 import { Button, Form } from 'react-bootstrap'
 
 const Formulario = () => {
+  const alertExito = () => {
+    Swal.fire({
+      title: 'Datos enviados!',
+      text: 'Los datos fueron enviados exitosamente.',
+      icon: 'success',
+      confirmButtonText: 'Continuar'
+    })
+  }
+  
+  const alertError = () => {
+    Swal.fire({
+      title: 'Datos incompletos!',
+      text: 'Debes completar todos los datos para continuar.',
+      icon: 'error',
+      confirmButtonText: 'Aceptar'
+    })
+  }
+
+
   return (
     <section>
       <Form className='bgForm d-flex flex-column p-2'>
@@ -27,7 +47,7 @@ const Formulario = () => {
           </Form.Group>
         </div>
 
-        <Button variant='success' className='btnEnviar align-self-end'>Enviar</Button>
+        <Button variant='success' className='btnEnviar align-self-end' onClick={alertError}>Enviar</Button>
 
       </Form>
     </section>
