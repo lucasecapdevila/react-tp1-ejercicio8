779f607d740bfe5d0bad663f4155ee4749a6bc89
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-13 18:37:20 -0300

    se ajusta inputs del formulario

--- components/Formulario.jsx
+++ components/Formulario.jsx
@@ -6,24 +6,24 @@ const Formulario = () => {
     <section>
       <Form className='bgForm d-flex flex-column p-2'>
         <div className="row">
-          <Form.Group className="mb-3 col-md-6" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Group className="mb-3 col-md-6" controlId="nombre">
+            <Form.Label>Nombre</Form.Label>
+            <Form.Control type="text" placeholder="Juan" />
           </Form.Group>
           
-          <Form.Group className="mb-3 col-md-6" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Group className="mb-3 col-md-6" controlId="apellido">
+            <Form.Label>Apellido</Form.Label>
+            <Form.Control type="text" placeholder="Perez" />
           </Form.Group>
 
-          <Form.Group className="mb-3 col-md-6" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Group className="mb-3 col-md-6" controlId="dni">
+            <Form.Label>DNI</Form.Label>
+            <Form.Control type="number" placeholder="99.999.999" />
           </Form.Group>
 
-          <Form.Group className="mb-3 col-md-6" controlId="exampleForm.ControlInput1">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Group className="mb-3 col-md-6" controlId="correo">
+            <Form.Label>Correo Electrónico</Form.Label>
+            <Form.Control type="email" placeholder="ejemplo@mail.com" />
           </Form.Group>
         </div>
 
